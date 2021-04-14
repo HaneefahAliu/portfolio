1c9e143d957208f943dfd5a62aa43a219bd9fbfc
HaneefahAliu <aliuhaneefah@gmail.com>
2021-04-14 11:23:21 +0100

    fixes

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navBar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
-  
